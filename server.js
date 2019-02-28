3682bdcf9c9309d5037352b452f9d26a774f5e58
Jon Gorman <contact@jongorman.top>
2019-02-27 22:39:25 -0600

    workNow

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.get("/wholefoods", async (req, res) =>{
   res.send(json)
 })
 
-// if(process.env.NODE_ENV === 'production') {
+if(process.env.NODE_ENV === 'production') {
 
 app.use(express.static(path.join(__dirname, '/client/build/')));
 
@@ -40,7 +40,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/client/build/index.html'));
   });
-// }
+}
 
 app.listen(PORT, function () {
   console.log(`Server is running on port:, ${PORT}`);
