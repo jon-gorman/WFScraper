a514d4ab9006838d68d8bc434c830b1f42916fb1
Jon Gorman <contact@jongorman.top>
2019-02-27 21:59:07 -0600

    server file

--- server.js
+++ server.js
@@ -34,12 +34,12 @@ app.get("/wholefoods", async (req, res, next) =>{
 
 
 
-if(process.env.NODE_ENV === 'production') {
+if(process.env.NODE_ENV ) {
 
   app.use(express.static('/client/build'));
 // //must be set like this so that it reloads with heroku deploy!!!
   app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname + '/client/build/index.html'));
+    res.sendFile(path.resolve(__dirname, '/client/build/index.html'));
   });
 }
 
