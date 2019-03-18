8827d5781119f3e5964b7eaf9716459748ab1607
Jon Gorman <contact@jongorman.top>
2019-03-18 09:18:40 -0500

    actingFunny

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const app = express();
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || '4000';
 const path = require('path');
-const http = require('http');
+// const http = require('http');
 
-setInterval(function() {
-  http.get("https://shrouded-meadow-95377.herokuapp.com");
-}, 600000); // every 5 minutes (300000)
+// setInterval(function() {
+//   http.get("https://shrouded-meadow-95377.herokuapp.com");
+// }, 600000); // every 5 minutes (300000)
 
 app.use(cors());
 // const wholeFoodsRoute = express.Router()
